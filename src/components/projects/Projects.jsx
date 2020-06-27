3ecb14b9b55a9ce8fbec2e71372d6d9b67beef49
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-27 16:45:43 -0500

    Header y Formulario para Crear Tareas

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Sidebar from "../layout/Sidebar";
+import Bar from "../layout/Bar";
+import FormTask from "../tasks/FormTask";
 
 const Projects = () => {
   return (
     <div className="contenedor-app">
       <Sidebar />
       <div className="seccion-principal">
+        <Bar />
         <main>
+          <FormTask />
           <div className="contenedor-tareas"></div>
         </main>
       </div>
