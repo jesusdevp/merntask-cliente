35f97c3469a5a97da79848b0d54bb3b8b2f9b535
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-27 12:30:40 -0500

    Estrucutura basica de componentes y routing

---
+++ src/components/auth/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <h1>Desde Nueva Cuenta</h1>;
+};
+
+export default SignUp;
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <h1>Desde Proyectos</h1>;
+};
+
+export default Projects;
