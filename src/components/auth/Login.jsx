cca70889235bcf92d66f4de072992f9e449c33da
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-27 13:17:02 -0500

    Componentes SignUp y Login Creados

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Desde Login</h1>;
+  //state para iniciar sesion
+  const [usuario, setUsuario] = useState({
+    email: "",
+    password: "",
+  });
+
+  //estraer de usuario
+
+  const { email, password } = usuario;
+
+  const handleChange = (e) => {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+        <Link to={"./signup"} className="enlace-cuenta">
+          Crear Cuenta
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const SignUp = () => {
-  return <h1>Desde Nueva Cuenta</h1>;
+const Signup = () => {
+  //state para registrarse
+  const [usuario, setUsuario] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    confirmar: "",
+  });
+
+  //estraer de usuario
+
+  const { nombre, email, password, confirmar } = usuario;
+
+  const handleChange = (e) => {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Crear Cuenta</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="campo-form">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              placeholder="Tu Nombre"
+              value={nombre}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="confirmar">Confirmar Password</label>
+            <input
+              type="password"
+              id="confirmar"
+              name="confirmar"
+              placeholder="Repite tu Password"
+              value={confirmar}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Registrarme"
+            />
+          </div>
+        </form>
+        <Link to={"/"} className="enlace-cuenta">
+          Volver a Iniciar Sesión
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default SignUp;
+export default Signup;
